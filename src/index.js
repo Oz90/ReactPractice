8bb9fba538a1f97de5d9e980be83b8d96e3b0fc9
Oz90 <gurr__@hotmail.com>
2020-11-21 10:41:21 +0100

    Practice react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Setup variables
+const author = "Barack Obama";
+const title = "A Promised Land";
+var img =
+  "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book job="developer" />
+      <Book title="Random title" number={22} />
+    </section>
+  );
+}
+
+const Book = props => {
+  return (
+    <article className="book">
+      <img src={img} />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
